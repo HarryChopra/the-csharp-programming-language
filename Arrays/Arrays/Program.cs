303c74b69d3ecddec60a3256444e70ba12854240
Harry Chopra <harrychopra@gmail.com>
2020-03-11 10:38:49 +0000

    Done

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace Arrays
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+class Program {
+    static void Main () {
+
+        int[] EvenNumbers = new int[3];
+        EvenNumbers[0] = 0;
+        EvenNumbers[1] = 2;
+        EvenNumbers[2] = 4;
+        // EvenNumbers[3] = 6; // Cant hold more than the length of the array
+        Console.WriteLine(EvenNumbers[3]);
     }
 }
