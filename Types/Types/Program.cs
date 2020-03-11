b60f9fc6f6952b6d11daa62408b8f08c86e912e9
Harry Chopra <harrychopra@gmail.com>
2020-03-11 00:18:05 +0000

    Done

---
+++ Types/Types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string Name = "\"Harry Chopra\"";
+        string DirPath = "\\etc\\hosts";
+
+        // Verbatim Literal - Escape sequences ignored. Greater Readibility.
+        string NewDirPath = @"\etc\hosts";
+        bool b = true;
+
+        //How to check Min and max value within VS
+        Console.WriteLine(int.MinValue);
+
+        // Escape sequence characters
+        Console.WriteLine(Name);
+
+        Console.WriteLine(DirPath);
+
+        Console.WriteLine(NewDirPath);
+        
+    }
+
+}
