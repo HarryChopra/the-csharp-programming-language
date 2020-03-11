454fd66c247c7e4a07f386fbe072751bb7c37168
Harry Chopra <harrychopra@gmail.com>
2020-03-11 00:18:34 +0000

    Done

---
+++ ConsoleRW/ConsoleRW/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your first name:");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name:");
+        string LastName = Console.ReadLine();
+
+        // String Concatenation
+        Console.WriteLine("Welcome to the training, " + FirstName + " " + LastName);
+
+        // Placeholder Syntax (Recommended)
+        Console.WriteLine("Welcome to the training, {0} {1}", FirstName, LastName);
+    }
+}
