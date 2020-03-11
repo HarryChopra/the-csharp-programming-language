6665e0a559d2334539d35d47fbc810e42e59ccdb
Harry Chopra <harrychopra@gmail.com>
2020-03-11 00:18:54 +0000

    Done

---
+++ CommonOperators/CommonOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int Number = 15;
+        //Ternary
+        bool isNumber = Number == 10 ? true : false;
+        Console.WriteLine(isNumber);
+    }
+}
+
