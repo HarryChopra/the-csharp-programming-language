0bd225ef9c6d0ba76ca72428f0279030b58f1d93
Harry Chopra <harrychopra@gmail.com>
2020-03-11 11:37:16 +0000

    Done

--- Switches/Switches/Program.cs
+++ Switches/Switches/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace Switches
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        StarBucks();
+    }
+
+    static void StarBucks()
+    {
+        Console.WriteLine("Welcome to Starbucks!");
+        Console.WriteLine("Please select your coffee serving size.");
+        Console.WriteLine("**Note: We are only serving caffe lattes today!");
+        Console.WriteLine(" 1 - Tall, 2 - Grande, 3 - Venti\n");
+
+        int UserInput = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nPlease enter the quantity:");
+        int UserQty = int.Parse(Console.ReadLine());
+        int total = 0;
+
+        switch (UserInput)
         {
-            Console.WriteLine("Hello World!");
+            case 1:
+                ProcessOrder("Tall");
+                break;
+            case 2:
+                ProcessOrder("Grande");
+                break;
+            case 3:
+                ProcessOrder("Venti");
+                break;
         }
+
+        void ProcessOrder(string sizeDes)
+        {
+            Console.WriteLine("\n{0} cups of caffe latte ({1}), coming up!", UserQty, sizeDes);
+            total = UserInput * UserQty;
+            Console.WriteLine("Your total is £{0}. Please pay at the counter. Thanks!", total);
+        }
+
     }
 }
+
