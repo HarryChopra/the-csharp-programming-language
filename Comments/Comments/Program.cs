5c6b2134c6e13586ee9e9298ab7f75d6a79e13f6
Harry Chopra <harrychopra@gmail.com>
2020-03-11 11:10:39 +0000

    Done

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        //Single line
+        // Console.WriteLine("Hello World!");
+
+        /* Multi-line
+         * Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!"); */
+
+
+        Console.WriteLine("Hello World!");
+
+        // XML documentation provides meaningful definition of classes et al
+        new SampleClass();
+
+    }
+}
+
+
+/// <summary>
+/// This is sample class and a sample description - XML documentaiton comments
+/// </summary>
+public class SampleClass
+{
+
+}
